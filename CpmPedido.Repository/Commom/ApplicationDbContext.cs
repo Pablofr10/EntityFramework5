1051517910e5fabe73a9ee628496690add06eaed
Pablofr10 <rafanet.live@gmail.com>
2021-02-07 14:48:14 -0300

    Adicionando Contexto

---
+++ CpmPedido.Repository/Commom/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CpmPedido.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+        public ApplicationDbContext()
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+
+    }
+}
